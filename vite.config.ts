7ea7b046c43ea39345d22389c3ddbeb9137e495b
Pedro Antonio <pedromila2000@gmail.com>
2024-07-04 15:03:52 -0300

    Deploy config saved

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/react_brazilianfood_ecommerce"
   server: {
     mimeTypes: {
       "application/typescript": ["ts"],
